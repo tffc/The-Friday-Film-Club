8b2dc3d83be81713c728a9d970af4a088402bf2d
Eli Stone <3li.stone@gmail.com>
2020-01-28 22:07:59 +0000

    More validation information and settings

--- web/modules/custom/tffc_validation/src/Controller/ValidationController.php
+++ web/modules/custom/tffc_validation/src/Controller/ValidationController.php
@@ -3,34 +3,111 @@
 namespace Drupal\tffc_validation\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityStorageException;
 use Drupal\node\NodeInterface;
+use Drupal\taxonomy\TermInterface;
+use Drupal\user\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ValidationController extends ControllerBase {
 
+  /**
+   * Field that holds a list of users that marked as valid
+   *
+   * @var string
+   */
+  private $field_validators = 'field_validator';
+
+  /**
+   * Field that holds a list of users that marked as invalid
+   *
+   * @var string
+   */
+  private $field_invalidators = 'field_invalid_user';
+
+  private $field_reasons = 'field_invalid_reasons';
+
+  /**
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *
+   * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+   */
+  public function page(Request $request) {
+
+    $data = $this->load_json('/films/json/validation/' . $this->current_uid());
+    $id = 0;
+
+    if (isset($data[0]) && isset($data[0]['id'])) {
+      $id = $data[0]['id'];
+    }
+
+    return $this->redirect('view.validation.validation_page', [
+      'nid' => $id,
+    ]);
+  }
+
+  /**
+   * @param \Drupal\node\NodeInterface $node
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   */
   public function success(NodeInterface $node, Request $request) {
-    dpm($node);
-
     $this->is_type_film($node);
 
-    return [
-      '#markup' => 'success',
-    ];
+    if ($this->has_already_validated($node)) {
+      \Drupal::messenger()
+        ->addStatus(t('You have already validated this film.'));
+      return $this->redirect_back();
+    }
+
+    $node->{$this->field_validators}[] = ['target_id' => $this->current_uid()];
+    try {
+      $node->save();
+    } catch (EntityStorageException $e) {
+      die($e->getMessage());
+    }
+
+    \Drupal::messenger()
+      ->addStatus(t('Thank you, marked film as valid.'));
+    return $this->redirect_back();
   }
 
+  /**
+   * @param \Drupal\node\NodeInterface $node
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   */
   public function skip(NodeInterface $node, Request $request) {
-    dpm($node);
-
     $this->is_type_film($node);
 
-    return [
-      '#markup' => 'skip',
-    ];
+    return $this->redirect_back();
   }
 
-  public function issue() {
 
+  public function issue(NodeInterface $node, TermInterface $term, Request $request) {
+    $this->is_type_film($node);
+    $this->is_issue_taxonomy($term);
+
+    if ($this->has_already_validated($node)) {
+      \Drupal::messenger()
+        ->addStatus(t('You have already validated this film.'));
+      return $this->redirect_back();
+    }
+
+    $node->{$this->field_invalidators}[] = ['target_id' => $this->current_uid()];
+    $node->{$this->field_reasons}[] = ['target_id' => $term->id()];
+    try {
+      $node->save();
+    } catch (EntityStorageException $e) {
+      die($e->getMessage());
+    }
+
+    \Drupal::messenger()
+      ->addStatus(t('Thank you, marked film as having some issues.'));
+    return $this->redirect_back();
   }
 
   /**
@@ -45,8 +122,62 @@ class ValidationController extends ControllerBase {
     return TRUE;
   }
 
-  private function redirect_back() {
+  private function is_issue_taxonomy(TermInterface $term) {
+    if ("invalid_reasons" !== $term->bundle()) {
+      throw new NotFoundHttpException();
+    }
+    return TRUE;
+  }
 
+  /**
+   * @param \Drupal\node\NodeInterface $node
+   *
+   * @return bool
+   */
+  private function has_already_validated(NodeInterface $node) {
+    $uid = $this->current_uid();
+
+    $validators = $node->get($this->field_validators)->getValue();
+    $ids = array_column($validators, 'target_id', 'target_id');
+    if (isset($ids[$uid])) {
+      return TRUE;
+    }
+
+    $invalidators = $node->get($this->field_invalidators)->getValue();
+    $ids = array_column($invalidators, 'target_id', 'target_id');
+    if (isset($ids[$uid])) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Redirects back to the validation page which will then sort out what to do
+   * next
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   */
+  private function redirect_back() {
+    return $this->redirect('tffc_validation.page');
+  }
+
+  /**
+   * @param $url
+   *
+   * @return mixed
+   */
+  private function load_json($url) {
+    $host = \Drupal::request()->getSchemeAndHttpHost();
+    $url = $host . $url;
+    return json_decode(file_get_contents($url), TRUE);
+  }
+
+  /**
+   * @return int
+   */
+  private function current_uid() {
+    return \Drupal::currentUser()->id();
   }
 
 }
--- web/modules/custom/tffc_validation/src/Plugin/Block/ValidationBlock.php
+++ web/modules/custom/tffc_validation/src/Plugin/Block/ValidationBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\tffc_validation\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Provides a 'Validation' Block.
@@ -19,9 +20,30 @@ class ValidationBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    return [
-      '#theme' => 'validation_options',
-    ];
+    $id = $this->get_film_id();
+    if($id) {
+      return [
+        '#theme' => 'validation_options',
+        '#film_id' => $id,
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ];
+    }
+  }
+
+  /**
+   * Update the status
+   *
+   * @return bool|mixed|null
+   */
+  private function get_film_id() {
+    $nid = \Drupal::routeMatch()->getParameter('nid');
+    $node = Node::load($nid);
+    if ($node && $node->bundle() === "film") {
+      return $nid;
+    }
+    return FALSE;
   }
 
 }
